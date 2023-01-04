5b8c891b855760dc27e3fd1e2f835e9b45ec1146
glebshatilov <Gleb.Shatilov@technopark.ru>
2023-01-04 20:20:49 +0700

    Added .env file

--- src/services/apollo/config/client.ts
+++ src/services/apollo/config/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: import.meta.env.VITE_GRAPHQL_URI,
   cache: new InMemoryCache(),
 })
 
