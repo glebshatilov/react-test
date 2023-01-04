10f7943b007e20bf0f27465046a5dc669ae4c950
glebshatilov <Gleb.Shatilov@technopark.ru>
2023-01-05 00:50:40 +0700

    Added MUI lib to project

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import router from './router'
 
 import { ApolloProvider } from '@apollo/client'
 import apolloClient from './services/apollo/config/client.js'
+import CssBaseline from '@mui/material/CssBaseline'
 
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
+      <CssBaseline />
       <RouterProvider router={router} />
     </ApolloProvider>
   )
