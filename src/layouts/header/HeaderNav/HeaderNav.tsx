154193f108dcaf65d3879fb5cd3e5b29491a6701
glebshatilov <Gleb.Shatilov@technopark.ru>
2023-02-19 23:34:16 +0700

    Changed nav links in headers. Now it's UI-components without underlining

--- src/layouts/header/HeaderNav/HeaderNav.tsx
+++ src/layouts/header/HeaderNav/HeaderNav.tsx
@@ -1,12 +1,34 @@
 import Stack from '@mui/material/Stack'
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
+import Link from '@mui/material/Link'
 
 function HeaderNav() {
   return (
     <Stack direction="row" spacing={2}>
-      <Link to="/">Home</Link>
-      <Link to="/">Articles</Link>
-      <Link to="/users">Users</Link>
+      <Link
+        component={RouterLink}
+        to="/"
+        underline="none"
+        color="gray"
+      >
+        Home
+      </Link>
+      <Link
+        component={RouterLink}
+        to="/"
+        underline="none"
+        color="gray"
+      >
+        Articles
+      </Link>
+      <Link
+        component={RouterLink}
+        to="/users"
+        underline="none"
+        color="gray"
+      >
+        Users
+      </Link>
     </Stack>
   )
 }
