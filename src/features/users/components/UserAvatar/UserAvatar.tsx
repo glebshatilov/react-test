358d3431dcfd765a6d577edc4d0a7fcbbc300054
glebshatilov <Gleb.Shatilov@technopark.ru>
2023-03-28 19:12:32 +0700

    Added opportunity to use avatar as a link to user profile page.

--- src/features/users/components/UserAvatar/UserAvatar.tsx
+++ src/features/users/components/UserAvatar/UserAvatar.tsx
@@ -1,5 +1,6 @@
 import Avatar from '@mui/material/Avatar'
 import { useAuth } from '@/features/auth/index.js'
+import { Link } from 'react-router-dom'
 
 interface Props {
   size?: {
@@ -7,15 +8,26 @@ interface Props {
     height: number
   }
   user?: any
+  link?: boolean
 }
 
-function UserAvatar({ size, user: propUser }: Props) {
+function UserAvatar({ size, user: propUser, link = false }: Props) {
   const { signedInUser } = useAuth()
   const user = propUser || signedInUser
   const avatarLetter = (user?.name || user?.email)[0].toUpperCase()
 
+  const avatarLinkAttributes = link
+    ? { component: Link, to: `/u/${user.id}`, style: { textDecoration: 'inherit' } }
+    : {}
+
   return (
-    <Avatar src={user?.avatar?.src || ''} sx={{ width: size?.width, height: size?.height }}>{avatarLetter}</Avatar>
+    <Avatar
+      {...avatarLinkAttributes}
+      src={user?.avatar?.src || ''}
+      sx={{ width: size?.width, height: size?.height }}
+    >
+      {avatarLetter}
+    </Avatar>
   )
 }
 
